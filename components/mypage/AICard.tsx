f2dd94c0291e598b6909da48c878d127ac62dfff
ddol9 <drddol9@gmail.com>
2024-10-13 14:46:44 +0900

    set tail image in mypage

--- components/mypage/AICard.tsx
+++ components/mypage/AICard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import avatarImage from "@/assets/avatar.png";
+import logoImg from "@/assets/taillogo.png";
 
 interface AICardProps {
   id: string;
@@ -22,13 +22,23 @@ const AICard: React.FC<AICardProps> = ({
     <div className="bg-[#2A2D36] rounded-lg p-4 mb-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center flex-grow mr-4">
-          <Image
-            src={profile_image_url || avatarImage}
-            alt={name}
-            width={48}
-            height={48}
-            className="rounded-full mr-3 flex-shrink-0"
-          />
+          {profile_image_url ? (
+            <Image
+              src={profile_image_url}
+              alt={name}
+              width={48}
+              height={48}
+              className="rounded-full mr-3 flex-shrink-0"
+            />
+          ) : (
+            <div className="w-[50px] h-[50px] flex items-center justify-center mr-3">
+              <Image
+                src={logoImg}
+                alt="Default Logo"
+                className="object-contain"
+              />
+            </div>
+          )}
           <div className="flex flex-col min-w-0">
             <h3 className="text-lg font-semibold text-white truncate max-w-[200px]">
               {name}
