cd0f52a4ee3d806a5aca2e65f3d1c91695c6b595
ddol9 <drddol9@gmail.com>
2024-10-13 13:45:15 +0900

    set tail image in search page

--- components/search/AICard.tsx
+++ components/search/AICard.tsx
@@ -4,6 +4,7 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
 import { AICardProps, CardData } from "@/utils/interface";
 import AIDetailsPopup from "../explore/AIDetailsPopup";
+import logoImg from "@/assets/taillogo.png";
 
 export const AICard = ({ item }: AICardProps) => {
   const router = useRouter();
@@ -17,18 +18,25 @@ export const AICard = ({ item }: AICardProps) => {
       <Dialog>
         <DialogTrigger asChild>
           <div className="flex items-center cursor-pointer flex-grow min-w-0 mr-4">
-            {item.profile_image_url ? (
-              <Image
-                src={item.profile_image_url}
-                alt={item.name}
-                width={50}
-                height={50}
-                className="rounded-full mr-4 flex-shrink-0"
-              />
-            ) : (
-              <div className="w-[50px] h-[50px] rounded-full bg-primary-900 mr-4 flex-shrink-0 flex items-center justify-center">
-              </div>
-            )}
+            <div className="flex-shrink-0 mr-4">
+              {item.profile_image_url ? (
+                <Image
+                  src={item.profile_image_url}
+                  alt={item.name}
+                  width={50}
+                  height={50}
+                  className="rounded-full object-cover"
+                />
+              ) : (
+                <div className="w-[50px] h-[50px] flex items-center justify-center">
+                  <Image
+                    src={logoImg}
+                    alt="Default Logo"
+                    className="object-contain"
+                  />
+                </div>
+              )}
+            </div>
             <div className="min-w-0 flex-grow">
               <h3 className="font-semibold truncate">{item.name}</h3>
               <p className="text-sm text-gray-600 truncate">{item.creator}</p>
@@ -36,7 +44,6 @@ export const AICard = ({ item }: AICardProps) => {
           </div>
         </DialogTrigger>
 
-        {/* Conditionally render AIDetailsPopup when the dialog is opened */}
         <DialogContent className="sm:max-w-[425px] rounded-3xl p-6 max-h-[80vh] overflow-y-auto">
           <AIDetailsPopup ai_detail={item} />
         </DialogContent>
